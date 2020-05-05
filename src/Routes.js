8de780e26f6ab68c8e323afd42c64bc16084a019
mj <hi.minjungkim@gmail.com>
2020-05-05 12:36:37 +0900

    update background-color

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./App";
-import CardDetail from "./component/CardDetail/CardDetail"
+import CardDetail from "./component/CardDetail/CardDetail";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
+          <Route exact path='/' compoenent={App} />
           <Route exact path='/colors' component={App} />
           <Route exact path='/color/:id' component={CardDetail} />
         </Switch>
